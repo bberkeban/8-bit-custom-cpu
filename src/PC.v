f8385d470c09463bf1a59cc3961a2cc00fe28177
Berkeban <ibberkeban@gmail.com>
2026-07-28 02:47:18 +0300

    debugged PC.v due to critical error in synthesis

--- src/PC.v
+++ src/PC.v
@@ -5,14 +5,11 @@ module PC_1 (
     input load,
     output reg [7:0] PC
 );
-    integer i;
 
 
     always @(posedge clk or posedge rst) begin
         if (rst == 1'b1) begin
-            for (i = 0; i < 8; i = i + 1) begin // the for loop is redundant, a basic <= 16'b0 is enough!
-                PC[i] <= 0;
-            end
+                PC <= 8'b00000000;
         end
         else if (load == 1'b1) begin
             PC <= I;
@@ -21,3 +18,5 @@ module PC_1 (
             PC <= PC + 1'b1;
         end
 endmodule
+
+// ltp of PC is determined as 35 without opt
